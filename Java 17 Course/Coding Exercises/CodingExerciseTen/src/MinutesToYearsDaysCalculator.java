9236e431ed3043cd14edf9add6b964832c677a45
Jhoser92 <58017514+Jhoser92@users.noreply.github.com>
2024-09-19 01:00:00 -0400

    added coding exercise ten

---
+++ Java 17 Course/Coding Exercises/CodingExerciseTen/src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,20 @@
+public class MinutesToYearsDaysCalculator {
+
+    public static void main(String[] args) {
+
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long year = minutes / 525600;
+            long remainingMinutes = minutes % 525600;
+            long day = remainingMinutes /  1440;
+            System.out.println(minutes + " min = " + year + " y and " + day + " d");
+
+        }
+    }
+}
